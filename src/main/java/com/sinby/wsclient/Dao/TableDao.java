c9e535cdffbcf1cf289869721d3aaf08a5e747b2
ASinby <sinby857@126.com>
2021-06-07 10:50:47 +0800

    插入轨道号

--- src/main/java/com/sinby/wsclient/Dao/TableDao.java
+++ src/main/java/com/sinby/wsclient/Dao/TableDao.java
@@ -2,11 +2,17 @@ package com.sinby.wsclient.Dao;
 
 import com.sinby.wsclient.Entity.TableVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
+
 @Repository
 public class TableDao {
+
+    @Value("${websocket.server.ip}")
+    private String IPS;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -18,7 +24,10 @@ public class TableDao {
      */
     public int addTable(TableVO tableVO){
 
-        return jdbcTemplate.update("INSERT INTO WLUSER.LADLE_WEIGHT_SIGNAL(SCALES_NO, LADLE_NO, WEIGHT, DIO_0, DIO_1) VALUES(?, ?, ?, ?, ?)",
-                tableVO.getScalesNo(), tableVO.getLadleNo(), tableVO.getWeight(), tableVO.getDIO_0(), tableVO.getDIO_1());
+        String[] arr = IPS.split(",");
+        int positon = Arrays.binarySearch(arr, tableVO.getScalesNo())+1;  //轨道号
+
+        return jdbcTemplate.update("INSERT INTO WLUSER.LADLE_WEIGHT_SIGNAL(SCALES_NO, LADLE_NO, WEIGHT, DIO_0, DIO_1, PATHWAY_NO) VALUES(?, ?, ?, ?, ?, ?)",
+                tableVO.getScalesNo(), tableVO.getLadleNo(), tableVO.getWeight(), tableVO.getDIO_0(), tableVO.getDIO_1(), positon);
     }
 }
